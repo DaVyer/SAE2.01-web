2b4e23ccec9931ea9155335f43573da3583c25cc
Raphael Gomes <raphael.gomes@etudiant.univ-reims.fr>
2023-06-07 11:08:07 +0200

    Ajout des classes Webpage et AppWebPage

---
+++ src/Html/AppWebPage.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Html;
+
+class AppWebPage extends WebPage
+{
+    public function __construct(string $title = '')
+    {
+        WebPage::__construct($title);
+    }
+
+    public function toHTML(): string
+    {
+        $html = <<<HTML
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{$this->getTitle()}</title>
+    {$this->getHead()}
+  </head>
+  
+  <body>
+    <div class="header">
+        <h1>{$this->getTitle()}</h1>
+    </div>
+  
+    <div class="content">
+        <div class='list'>{$this->getBody()}</div>
+    </div>
+  
+    <div class="footer">
+        <div class="modif">{$this->getLastModification()}</div>
+    </div>
+  </body>
+    
+</html>
+HTML;
+        return $html;
+    }
+}
