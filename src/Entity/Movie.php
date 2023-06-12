1593d5708fba59b029d49774ba2f9bf81a2ade36
Raphael Gomes <raphael.gomes@etudiant.univ-reims.fr>
2023-06-12 10:22:27 +0200

    Création du répertoire Entity et définition des attributs de la classe Movie

---
+++ src/Entity/Movie.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+class Movie
+{
+
+    private int $posterId;
+    private string $originalLanguage;
+    private string $originalTitle;
+    private string $overview;
+    private string $releaseDate;
+    private int $runtime;
+    private string $tagline;
+    private string $title;
+    private int $id;
+
+
+}
