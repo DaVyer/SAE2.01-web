5c98e8eed20a4258811f57860c9c751a745825af
Raphael Gomes <raphael.gomes@etudiant.univ-reims.fr>
2023-06-12 18:39:21 +0200

    Modification de People.php pour ajouter une condition if

--- src/Entity/People.php
+++ src/Entity/People.php
@@ -72,21 +72,27 @@ class People
         return $this->id;
     }
 
-    public function getCover(): Cover
+    public function getCover(): ?Cover
     {
         $stmt = MyPdo::getInstance()->prepare(
             <<<'SQL'
-            SELECT *
-            FROM image
-            WHERE id = :imageId
-        SQL
+        SELECT *
+        FROM image
+        WHERE id = :imageId
+    SQL
         );
 
         $stmt->bindValue(':imageId', $this->avatarId, PDO::PARAM_INT);
         $stmt->setFetchMode(PDO::FETCH_CLASS, "Entity\Cover");
         $stmt->execute();
 
-        return $stmt->fetch();
+        $cover = $stmt->fetch();
+
+        if ($cover !== false) {
+            return $cover;
+        } else {
+            return null;
+        }
     }
 
 }
