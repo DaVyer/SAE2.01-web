753eced8ca7d65bf28def95c7eabefccb18c0762
Raphael Gomes <raphael.gomes@etudiant.univ-reims.fr>
2023-06-12 11:14:47 +0200

    Ajout des getters a la classe Cover.php

--- src/Entity/Cover.php
+++ src/Entity/Cover.php
@@ -7,6 +7,22 @@ class Cover
 {
     private string $jpeg;
     private int $id;
-    
+
+    /**
+     * @return string
+     */ 
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
 
 }
