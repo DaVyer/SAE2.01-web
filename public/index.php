9d79a70e622d5602056438c26ad17001a5f83028
Gwendal Rodrigues <gwendal.rodrigues@etudiant.univ-reims.fr>
2023-06-12 11:12:18 +0200

    Réalisation du fichier index.php terminé, sans css.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,21 @@
 <?php
+
 declare(strict_types=1);
 
+use Html\AppWebPage;
+use Entity\Movie;
+use Entity\Collection\MovieCollection;
+
+$webPage = new AppWebPage();
+
+$webPage->setTitle("Film");
+$webPage->appendCssUrl("public/style.css");
+
+$listeFilm = (new Entity\Collection\MovieCollection())->findAll();
+
+foreach ($listeFilm as $film) {
+    $moviesName = $webPage->escapeString("{$film->getTitle()}");
+    $webPage->appendContent("<a href='movie.php?movieId={$film->getId()}'>".$moviesName."</a><br>");
+}
+
+echo $webPage->toHTML();
