e4197c972c5627acbb921f9e63b81eb6167e9685
Raphael Gomes <raphael.gomes@etudiant.univ-reims.fr>
2023-06-12 14:21:29 +0200

    Définition des attributs de la classe Cast

---
+++ src/Entity/Cast.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+class Cast
+{
+    private int $movieId;
+    private int $peopleId;
+    private string $role;
+    private int $orderIndex;
+    private int $id;
+
+}
