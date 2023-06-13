fb3720974f9c1e7ca0fdcd6be7ff401664d24e9f
Raphael Gomes <raphael.gomes@etudiant.univ-reims.fr>
2023-06-14 00:18:28 +0200

    Ajout du fichier Modification.php qui interprète le formulaire pour créer ou modifier un film

---
+++ public/Modification.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+use Database\MyPdo;
+use Entity\Movie;
+use Html\AppWebPage;
+
+$webPage = new AppWebPage();
+
+$webPage->setTitle("Création ou modification d'un film");
+
+$webPage->appendCssUrl("css/style.css");
+
+
+if (isset($_POST['title'])) {
+    $title = $_POST['title'];
+
+    $stmt = MyPdo::getInstance()->prepare(<<<'SQL'
+                                SELECT * FROM movie WHERE title = :title
+                                SQL);
+    $stmt->bindValue(':title', $title, PDO::PARAM_STR);
+    $stmt->setFetchMode(PDO::FETCH_CLASS, "Entity\Movie");
+    $stmt->execute();
+    $movie = $stmt->fetch();
+
+
+    $title = $_POST['title'];
+    $originalLanguage = $_POST['originalLanguage'];
+    $originalTitle = $_POST['originalTitle'];
+    $releaseDate = $_POST['releaseDate'];
+    $runtime = intval($_POST['runtime']);
+
+    if ($movie !== false) {
+        if (isset($_POST['posterId'])) {
+            $posterId = !empty($_POST['posterId']) ? (int)$_POST['posterId'] : $movie->getPosterId();
+        } else {
+            $posterId = $movie->getPosterId();
+        }
+    } else {
+        $posterId = null;
+    }
+
+    if (isset($_POST['overview'])) {
+        $overview = $_POST['overview'];
+    } else {
+        $overview = $movie->getOverview();
+    }
+
+    if (isset($_POST['tagline'])) {
+        $tagline = $_POST['tagline'];
+    } else {
+        $tagline = $movie->getTagline();
+    }
+
+    if ($movie) {
+        $movie->setTitle($title);
+        $movie->setOriginalLanguage($originalLanguage);
+        $movie->setOriginalTitle($originalTitle);
+        $movie->setReleaseDate($releaseDate);
+        $movie->setRuntime($runtime);
+        $movie->setOverview($overview);
+        $movie->setTagline($tagline);
+
+        $movie->update();
+
+        $webPage->appendContent("<p>Le film a été modifié avec succès.</p>");
+    } else {
+        $newMovie = Movie::create($title, $originalLanguage, $originalTitle, $overview, $releaseDate, $runtime, $tagline, $posterId);
+
+        $newMovie->insert();
+
+        $webPage->appendContent("<p>Le film a été ajouté avec succès.</p>");
+    }
+} else {
+    $webPage->appendContent("<p>Aucune donnée n'a été soumise.</p>");
+}
+
+echo $webPage->toHTML();
