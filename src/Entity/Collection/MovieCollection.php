8ee000038d60d66eb3ac426e9b6768d2537fc954
Raphael Gomes <raphael.gomes@etudiant.univ-reims.fr>
2023-06-14 13:51:52 +0200

    Ajout d'une méthode pour trouver les films en fonction du genre

--- src/Entity/Collection/MovieCollection.php
+++ src/Entity/Collection/MovieCollection.php
@@ -38,4 +38,26 @@ class MovieCollection
 
         return $stmt->fetchAll();
     }
+
+    public function findByGenreId(?string $genreId): array
+    {
+        if ($genreId === null || $genreId === "all") {
+            return $this->findAll();
+        }
+
+        $query = "
+        SELECT m.*
+        FROM movie m
+        INNER JOIN movie_genre mg ON m.id = mg.movieId
+        WHERE mg.genreId = :genreId
+        ORDER BY m.title
+    ";
+
+        $stmt = myPdo::getInstance()->prepare($query);
+        $stmt->bindValue(':genreId', $genreId);
+        $stmt->setFetchMode(\PDO::FETCH_CLASS, "Entity\Movie");
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
 }
