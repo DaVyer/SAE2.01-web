d7f5cac0f057ad7c0eb8359d8713cfcb1edc8289
Gwendal Rodrigues <gwendal.rodrigues@etudiant.univ-reims.fr>
2023-06-14 12:24:26 +0200

    Ajout de la classe Genre.php et de ses accesseurs

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -4,5 +4,24 @@ namespace Entity;
 
 class Genre
 {
+    public string $name;
+    public int $id;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
 
 }
