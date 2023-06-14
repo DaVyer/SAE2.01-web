004c277db5a10c849b485928f7aa24cfaba66153
Gwendal Rodrigues <gwendal.rodrigues@etudiant.univ-reims.fr>
2023-06-14 13:48:26 +0200

    Fin du formulaire avec le css

--- public/edition.php
+++ public/edition.php
@@ -7,41 +7,58 @@ use Html\AppWebPage;
 
 $webPage = new \Html\AppWebPage();
 
-$webPage->setTitle("Formulaire de création, d'édition ou de suppression d'un film");
+$webPage->setTitle("Formulaire de création ou d'édition");
 
 $webPage->appendCssUrl("css/style.css");
 
 $webPage->appendContent("
 <form method='POST' class='formulaire' action='Modification.php'>
-    <div class='form__input'>
+    <div class='form__content'>
         <label for='title'>Entrer un titre de film
             <input type='text' name='title' id='title' required>
         </label>
+    </div>
+    <div class='form__content'>
         <label for='originalLanguage'>Veuillez rentrer une version original
             <input type='text' name='originalLanguage' id='originalLanguage' required>
         </label>
+    </div>
+    <div class='form__content'>
         <label for='originalTitle'>Veuillez rentrer un titre original
             <input type='text' name='originalTitle' id='originalTitle' required>
         </label>
+    </div>
+    <div class='form__content'>
         <label for='overview'>Veuillez rentrer un résumé
             <input type='text' name='overview' id='overview'>
         </label>
+    </div>
+    <div class='form__content'>
         <label for='posterId'>Veuillez rentrer un identifiant d'affiche
-            <input type='text' name='posterId' id='posterId'>
+            <input type='text' name='posterId' id='posterId' accept='image/png'>
         </label>
-        <label for='releaseDate'>Veuillez rentrer une date de sortie
+    </div>
+    <div class='form__content'>
+        <label for='releaseDate'>Veuillez choisir une affiche
             <input type='text' name='releaseDate' id='releaseDate' required>
         </label>
+    </div>    
+    <div class='form__content'>
         <label for='runtime'>Veuillez rentrer la durée du film
             <input type='text' name='runtime' id='runtime' required>
         </label>
-        <label for='tagline'>Veuillez rentrer la slogan du film
+    </div>
+    <div class='form__content'>
+        <label for='tagline'>Veuillez rentrer le slogan du film
             <input type='text' name='tagline' id='tagline'>
         </label>
     </div>
-    <input type='submit' name='submit__form' id='submit__form'>
+    <div class='form__button'>
+        <input type='submit' name='submit__form' id='submit__form'>
+    </div>
 </form>
 ");
+
 
 
 echo $webPage->toHTML();
