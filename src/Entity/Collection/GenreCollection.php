b0d22c445d97f8ba075e21529c49681eeeaf740d
Gwendal Rodrigues <gwendal.rodrigues@etudiant.univ-reims.fr>
2023-06-14 12:24:58 +0200

    Ajout de la classe GenreCollection et de sa méthode findAll()

--- src/Entity/Collection/GenreCollection.php
+++ src/Entity/Collection/GenreCollection.php
@@ -2,7 +2,21 @@
 
 namespace Entity\Collection;
 
+use Database\MyPdo;
+
 class GenreCollection
 {
+    public function findAll(): array
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<SQL
+            SELECT *
+            FROM genre
+            ORDER BY name, id
+        SQL
+        );
+        $stmt->execute();
 
+        return $stmt->fetchAll(PDO::FETCH_CLASS, "Entity\Genre");
+    }
 }
